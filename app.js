914743b6446247d9fd212369e57df20520996413
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2020-04-14 21:45:42 -0700

    request to work and show array

--- app.js
+++ app.js
@@ -1,46 +1,45 @@
 function getGithubUser(textInputVal) {
+    const url = `https://api.github.com/users/${textInputVal}/repos`;
   
-    const url = `https://dog.ceo/api/breed/${textInputVal}/images/random/1`;
-    console.log(url);
     const head = {
-        headers: new Headers({
-          "X-Api-Key": apiKey,
-        }),
-      };
-      fetch(url)
-        .then(response => response.json())
-        .then(responseJson => displayResults(responseJson))
-        .catch(error => alert('Something went wrong. Try again later.')
-        );
+      headers: new Headers({
+        'Accept': "application/vnd.github.v3+json",
+      }),
+    };
+  
+    console.log(url);
+
+    fetch(url, head)
+      .then((response) => response.json())
+      .then((responseJson) => displayResults(responseJson))
+      .catch((error) => alert("Something went wrong. Try again later."));
+  }
+  
+  function displayResults(responseJson) {
+    console.log(responseJson);
+
+    if (responseJson.status == "error") {
+      $(".results-img").replaceWith(`<p> dog not found</p>`);
+    } else {
+      $(".results-img").html(
+        `<h1>${responseJson[0].owner[0]}</h1>`
+      );
+  
+      $(".results").removeClass("hidden");
     }
+  }
   
-    function displayResults(responseJson) {
-      console.log(responseJson);
-      if(responseJson.status == 'error'){
-        $('.results-img').replaceWith(
-           `<p> dog not found</p>`
-        )
-      }else{
-        $('.results-img').replaceWith(
-          `<img src="${responseJson.message}" class="results-img">`
-        )
-        
-        $('.results').removeClass('hidden');
-  
-      }
-  
-    }
-  
-    function watchForm() {
-      $('form').submit(event => {
-        event.preventDefault();
-        let textInputVal = $('#').val();
-        console.log(textInputVal);
-        getGithubUser(textInputVal);
-      });
-    }
-    
-    $(function() {
-      console.log('App loaded! Waiting for submit!');
-      watchForm();
+  function watchForm() {
+    $("form").submit((event) => {
+      event.preventDefault();
+      let textInputVal = $("#github-search").val();
+      console.log(textInputVal);
+      $("#github-search").val(" ");
+      getGithubUser(textInputVal);
     });
+  }
+  
+  $(function () {
+    console.log("App loaded! Waiting for submit!");
+    watchForm();
+  });
